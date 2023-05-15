a54b77428c191cad55516675c166174b300f5f5a
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-16 00:09:08 +0300

    Update freq_in_file.py

--- src/freq_in_file.py
+++ src/freq_in_file.py
@@ -1,24 +1,36 @@
+import os
+
 from aligment import find_best_alignment
 from build_dict_from_scalop import get_dict_from_scalop
 from build_frame import build_frame
 from csv_reader import read_csv_file
 from json_writer import data_write_in_json
 from typing import List, Dict
+from write_fasta_file import write_fasta_file
 
 
-def get_freq_in_file_for_cdr(file_input: str, file_output: str) -> None:
+def get_freq_in_file(file_input: str, file_output: str) -> None:
     sequences: List[Dict[str, str]] = read_csv_file(file_input)
 
+    first_sequence = sequences[0]["sequence"]
+    length_of_first_sequence = len(first_sequence)
+
+    list_for_fasta = []
     for seq in sequences:
+        sequence: str = seq["sequence"]
         if seq["chain"] is not None:
-            sequence: str = seq["sequence"]
+
             chain: str = seq["chain"]
 
             print(f"Accepted sequences: {seq['sequence']}, chain: {seq['chain']}")
 
             seq_for_scalop: str = build_frame(sequence, chain)
-            my_dict: Dict[str, List[Dict[str, str]]] = get_dict_from_scalop(seq_for_scalop, chain=chain)
+            list_for_fasta.append(seq_for_scalop)
 
+            write_fasta_file("sequences.fasta", list_for_fasta)
+
+            my_dict: Dict[str, List[Dict[str, str]]] = get_dict_from_scalop("sequences.fasta", chain=chain)
+            os.remove("sequences.fasta")
             if len(my_dict) == 0:
                 print(
                     f"There is no frequency information for these sequences: {seq['sequence']} and chain: {seq['chain']}")
@@ -28,9 +40,25 @@ def get_freq_in_file_for_cdr(file_input: str, file_output: str) -> None:
                 true_family: str = key
                 true_cdr: str = value[0]['cdr_sequence']
 
-                print(f"Frequencies for CDR '{true_cdr} from {true_family} canonical form writing in output file")
+                print(f"Frequencies for CDR '{true_cdr} from {true_family} canonical form writing in output file'")
 
                 data_write_in_json(true_cdr, true_family, file_output)
+        elif length_of_first_sequence > 20:
+
+            print(f"Accepted sequences: {seq['sequence']}")
+
+            list_for_fasta.append(sequence)
+            write_fasta_file("sequences.fasta", list_for_fasta)
+
+            my_dict: Dict[str, List[Dict[str, str]]] = get_dict_from_scalop("sequences.fasta", flag=True)
+            os.remove("sequences.fasta")
+            for key, value in my_dict.items():
+                true_family: str = key
+                true_cdr: str = value[0]['cdr_sequence']
+                print(f"Frequencies for CDR {true_cdr} from {true_family} canonical form writing in output file'")
+
+                data_write_in_json(true_cdr, true_family, file_output)
+
         else:
             true_cdr: str = seq["sequence"]
             true_family: str = find_best_alignment(true_cdr)
@@ -41,18 +69,3 @@ def get_freq_in_file_for_cdr(file_input: str, file_output: str) -> None:
             data_write_in_json(true_cdr, true_family, file_output)
 
 
-def get_freq_in_file_for_seq(file_input: str, file_output: str) -> None:
-    sequences: List[Dict[str, str]] = read_csv_file(file_input)
-
-    for seq in sequences:
-        sequence: str = seq["sequence"]
-
-        print(f"Accepted sequences: {seq['sequence']}")
-
-        my_dict: Dict[str, List[Dict[str, str]]] = get_dict_from_scalop(sequence, flag=True)
-        for key, value in my_dict.items():
-            true_family: str = key
-            true_cdr: str = value[0]['cdr_sequence']
-            print(f"Frequencies for CDR {true_cdr} from {true_family} canonical form writing in output file'")
-
-            data_write_in_json(true_cdr, true_family, file_output)
