87df56bb856a2d6e2f8de9f05f93a8adc3ab66b8
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-16 13:43:04 +0300

    Update mutant.py

--- src/mutant.py
+++ src/mutant.py
@@ -1,23 +1,20 @@
-import time
-import logging
+import json
 from typing import Optional
+import time
+import os
+import logging
 
+from combinations import get_combinations_fast
+from positions_mutant_aa import find_mutation_position
 from build_dict_from_scalop import get_dict_from_scalop
 from build_frame import build_frame
-from itertools import product
-
+from write_fasta_file import write_fasta_file
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def generate_combinations(input_string, alphabet):
-    possible_replacements = [alphabet] * len(input_string)
-    combinations = product(*possible_replacements)
-    return [''.join(c) for c in combinations if c != input_string]
-
-
-def get_all_mutant(seq: str, chain: str, output_file: str = "mutant_aa_result.txt") -> Optional[None]:
+def get_all_mutant(seq: str, chain: str, output_file: str = "mutant_aa_result.json") -> Optional[None]:
     start_time = time.time()
     true_family = None
     true_cdr = None
@@ -29,29 +26,36 @@ def get_all_mutant(seq: str, chain: str, output_file: str = "mutant_aa_result.tx
         true_cdr = value[0]['cdr_sequence']
         break
 
-    ALPHABET = "ACDEFGHIKLMNPQRSTVWY"
-
-    combinations = generate_combinations(seq, ALPHABET)
+    combinations = get_combinations_fast(seq)
     logger.debug('List of combinations: %s', combinations)
 
-    logger.info('Working with SCALOP')
-    result: set = set()
+    list_for_fasta = []
+
     for seq in combinations:
         frame_seq = build_frame(seq, chain)
-        mutant_dict = get_dict_from_scalop(frame_seq, chain=chain)
+        list_for_fasta.append(frame_seq)
 
-        for mutant_key, mutant_value in mutant_dict.items():
-            mutant_cdr = mutant_value[0]['cdr_sequence']
-            if mutant_key == true_family and mutant_cdr != true_cdr:
-                result.add(mutant_cdr)
+    logger.info('Writing fasta file')
+    write_fasta_file("sequences.fasta", list_for_fasta)
+    mutant_dict = get_dict_from_scalop("sequences.fasta", chain=chain)
+
+    logger.info('Building mutant dict')
+    result: dict = {}
+    for mutant_key, mutant_value in mutant_dict.items():
+        for mutant in mutant_value:
+            mutant_seq = mutant['cdr_sequence']
+            if mutant_key == true_family and mutant_seq != true_cdr:
+                mutation_pos = find_mutation_position(true_cdr, mutant_seq)
+                if mutation_pos not in result:
+                    result[mutation_pos] = []
+                result[mutation_pos].append(mutant_seq)
 
     logger.info('Writing output file')
     with open(output_file, 'w') as f:
-        for item in result:
-            f.write(item + '\n')
+        json.dump(result, f, indent=4)
 
     end_time = time.time()
     logger.info(f'Total time: {end_time - start_time}')
 
+    os.remove("sequences.fasta")
     return None
-
