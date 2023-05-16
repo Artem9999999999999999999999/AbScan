0422b59456fe6a651f691e504449b469cee8d077
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-16 14:07:02 +0300

    Update write_fasta_file.py

--- src/write_fasta_file.py
+++ src/write_fasta_file.py
@@ -1,8 +1,8 @@
+from typing import List
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 
-
-def write_fasta_file(filename, sequences):
+def write_fasta_file(filename: str, sequences: List[str]) -> None:
     records = [SeqRecord(Seq(seq), id=f"sequence{i+1}", description="") for i, seq in enumerate(sequences)]
     SeqIO.write(records, filename, "fasta")
