b68e693952b693e2ac0dfc1671baa3c041fa2504
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-19 23:49:35 +0300

    Update combinations.py

--- src/combinations.py
+++ src/combinations.py
@@ -12,3 +12,19 @@ def get_combinations_fast(seq: str) -> List[str]:
                 valid_combos.append(combo) # добавляем допустимую комбинацию в список
 
     return valid_combos
+
+
+def get_combinations_with_exclude(seq: str, exclude_positions: List[int]) -> List[str]:
+    alphabet = "ACDEFGHIKLMNPQRSTVWY" # 20 стандартных аминокислот
+    valid_combos = [] # список для хранения допустимых комбинаций
+
+    for i in range(len(seq)):
+        if i + 1 in exclude_positions:  # проверяем, является ли текущая позиция запрещенной
+            valid_combos.append(seq[i])  # если да, то добавляем текущую аминокислоту без изменений
+        else:
+            for a in alphabet:
+                if a != seq[i]:
+                    combo = seq[:i] + a + seq[i+1:] # создаем новую комбинацию, заменяя текущую аминокислоту на a
+                    valid_combos.append(combo) # добавляем допустимую комбинацию в список
+
+    return valid_combos
