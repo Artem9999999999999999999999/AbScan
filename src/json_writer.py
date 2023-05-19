9a394e2aa74beffa892f7054ac4a51daaf67b21d
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-20 00:01:14 +0300

    Update json_writer.py

--- src/json_writer.py
+++ src/json_writer.py
@@ -8,13 +8,10 @@ logger = logging.getLogger(__name__)
 def data_write_in_json(cdr: str, family: str, file_output: str) -> None:
     with open('../data_abscan/freq_aa_in_canonical_form.json', 'r') as f:
         data = json.load(f)
-    # Получаем данные для true_family из словаря data
+        
     data_for_true_family = data.get(family, [])
-
-    # Создаем словарь с данными для записи в файл
     data_to_write = {cdr: data_for_true_family}
 
-    # Записываем данные в файл file_output
     with open(file_output, 'a') as f:
         json.dump(data_to_write, f, indent=4)
         f.write('\n')
