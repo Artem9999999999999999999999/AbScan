dd28a00478436db30f61dcc6b1f98a35e03f61b2
Artem_Levonovich <113240536+Artem9999999999999999999999@users.noreply.github.com>
2023-05-20 00:00:44 +0300

    Update combinations.py

--- src/combinations.py
+++ src/combinations.py
@@ -2,29 +2,29 @@ from typing import List
 
 
 def get_combinations_fast(seq: str) -> List[str]:
-    alphabet = "ACDEFGHIKLMNPQRSTVWY" # 20 стандартных аминокислот
-    valid_combos = [] # список для хранения допустимых комбинаций
+    alphabet = "ACDEFGHIKLMNPQRSTVWY" 
+    valid_combos = [] 
 
     for i in range(len(seq)):
         for a in alphabet:
             if a != seq[i]:
-                combo = seq[:i] + a + seq[i+1:] # создаем новую комбинацию, заменяя текущую аминокислоту на a
-                valid_combos.append(combo) # добавляем допустимую комбинацию в список
+                combo = seq[:i] + a + seq[i+1:] 
+                valid_combos.append(combo)
 
     return valid_combos
 
 
 def get_combinations_with_exclude(seq: str, exclude_positions: List[int]) -> List[str]:
-    alphabet = "ACDEFGHIKLMNPQRSTVWY" # 20 стандартных аминокислот
-    valid_combos = [] # список для хранения допустимых комбинаций
+    alphabet = "ACDEFGHIKLMNPQRSTVWY" 
+    valid_combos = [] 
 
     for i in range(len(seq)):
-        if i + 1 in exclude_positions:  # проверяем, является ли текущая позиция запрещенной
-            valid_combos.append(seq[i])  # если да, то добавляем текущую аминокислоту без изменений
+        if i + 1 in exclude_positions:  
+            valid_combos.append(seq[i])  
         else:
             for a in alphabet:
                 if a != seq[i]:
-                    combo = seq[:i] + a + seq[i+1:] # создаем новую комбинацию, заменяя текущую аминокислоту на a
-                    valid_combos.append(combo) # добавляем допустимую комбинацию в список
+                    combo = seq[:i] + a + seq[i+1:] 
+                    valid_combos.append(combo) 
 
     return valid_combos
